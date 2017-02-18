f605ccbef1e43d6b4da45c3f3c80f0caea363513
ibreaker14 <minijordon@gmail.com>
2017-02-18 14:39:02 -0800

    made changes to prime_algorithm

--- Part Two/part2.py
+++ Part Two/part2.py
@@ -7,27 +7,37 @@ from datetime import datetime
 # CECS 424
 
 # returns whether input is prime
-def is_prime(input, divisor = 3):
+# param:	input: int number to be checked
+		#	divisor_a: smallest divisor to be compared with
+		#	divisor_b: largest divisor to be compared with
+def is_prime(input, divisor_a = 3, divisor_b = 0):
+	if divisor_a == 3:
 
-	if input == 2: 			# if input is 2, it is prime
+		# prime rule: a number N is the square of a number x. 
+		# Other factors of N is comprised of a number a that is larger than x 
+		# and a number b that is smaller than x or a and b is equal to x	
+		divisor_b = math.floor(math.sqrt(input))
+		if divisor_b % 2 == 0: 
+			divisor_b = divisor_b + 1
+
+	if input == 2: 				# if input is 2, it is prime
 		prime = True
-	elif input % 2 == 0:	# if divisible by 2, it is not prime
+	elif input % 2 == 0:		# if divisible by 2, it is not prime
 		prime = False
-	elif input < 2:			# if input is less than 2, it is not prime
+	elif input < 2:				# if input is less than 2, it is not prime
 		prime = False
-	elif divisor * divisor > input:  	# prime rule: a number N is the square of a number x. 
-										# Other factors of N is comprised of a number a that is larger than x 
-										# and a number b that is smaller than x or a and b is equal to x
+	elif divisor_a > divisor_b:	# if a and b meet, it is prime
 		prime = True
-	elif (input % divisor) == 0:	# if input can be modded, it is not prime
+	elif (input % divisor_a) == 0 or (input % divisor_b ) == 0:	# if input can be modded, it is not prime
 		prime = False
 	else:  							# look for primes with odd divisors
-		prime = is_prime(input, divisor = divisor + 2)
+		prime = is_prime(input, divisor_a + 2, divisor_b  - 2)
 	return prime
 
 # returns list of primes
+# param:	n: starting number 
 def get_primes(n):
-	startValue = 0
+	startValue = 0			# number to start counting from
 	maxValue = 2000000		# max value is two million
 	list = []				# declare list
 	if n < 2:				#special case: 1 is not prime but 2 is
@@ -48,6 +58,7 @@ def get_primes(n):
 	return list
 
 # returns the sum of a list of primes
+# param:	n: starting number up to 2 million
 def sum_primes(n):
 	sum = 0
 	list = get_primes(n)
@@ -62,7 +73,7 @@ startTime = datetime.now()	# starts timer
 startVal = 0
 print("sum of primes from",startVal,"to 2000000: ", sum_primes(startVal))
 
-# print(datetime.now() - startTime) # uncomment to view calculated execution time
+print("Calculated execution time: ",datetime.now() - startTime) # calculated execution time
 
 # keeps console alive
 os.system("pause")
